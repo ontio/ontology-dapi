3748869af771bbe0af17cf724b92e49865e56552
MickeyWang <1244134672@qq.com>
2019-06-12 16:57:23 +0800

    update returned value for state channel api

--- src/client/stateChannel.ts
+++ src/client/stateChannel.ts
@@ -1,13 +1,13 @@
 import { StateChannelApi } from '../api/stateChannel';
-import { Response } from '../api/types';
+import { Signature } from '../api/types';
 import { call } from './proxy';
 
 export const stateChannelApi: StateChannelApi = {
   login(password) {
-    return call<Response>('stateChannel.login', password);
+    return call<string>('stateChannel.login', password);
   },
 
   sign(args) {
-    return call<Response>('stateChannel.sign', args);
+    return call<Signature>('stateChannel.sign', args);
   }
 };
