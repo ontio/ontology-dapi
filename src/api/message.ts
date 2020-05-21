9667df02417a03ffbfda0f24cd76b54d7bff6418
MickWang <1244134672@qq.com>
2020-05-21 17:30:55 +0800

    update sign message api

--- src/api/message.ts
+++ src/api/message.ts
@@ -5,17 +5,19 @@ export interface MessageApi {
    * Initiates signing of arbitrary messageHash by the user account or identity.
    *
    * @param messageHash Hex encoded message hash
+   * @param useIdentity If sign with identity
    * @returns Hex encoded signature with hex encoded public key
    *
    * @throws NO_ADDRESS, MALFORMED_MESSAGE
    */
-  signMessageHash({ messageHash }: { messageHash: string }): Promise<Signature>;
+  signMessageHash({ messageHash, useIdentity }: { messageHash: string, useIdentity?: boolean }): Promise<Signature>;
 
   /**
    * Verifies that the signature is valid signature of messageHash.
    *
    * @param messageHash Hex encoded message hash
    * @param signature Hex encoded signature with hex encoded public key
+   * @param useIdentity If verify with identity
    *
    * @throws MALFORMED_MESSAGE, MALFORMED_SIGNATURE
    */
@@ -25,16 +27,18 @@ export interface MessageApi {
    * Initiates signing of arbitrary message by the user account or identity.
    *
    * @param message Text message
+   * @param useIdentity If sign with identity
    * @returns Hex encoded signature with hex encoded public key
    *
    * @throws NO_ADDRESS, MALFORMED_ADDRESS
    */
-  signMessage({ message }: { message: string }): Promise<Signature>;
+  signMessage({ message, useIdentity }: { message: string, useIdentity?: boolean }): Promise<Signature>;
 
   /**
    * Verifies that the signature is valid signature of message.
    *
    * @param message Text message
+   * @param useIdentity If verify with identity
    * @param signature Hex encoded signature with hex encoded public key
    *
    * @throws MALFORMED_SIGNATURE
