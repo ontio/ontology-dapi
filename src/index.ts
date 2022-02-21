fe06dffe47390674a0184501fa7aad6ead5204f9
zeyuyuan <yzy@pcomg.com>
2022-02-21 15:35:24 +0800

    feat: export utilsApi

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import * as client from './client/client';
+import { utilsApi } from './client/utils';
 import * as provider from './provider';
 import { MethodType, Rpc } from './rpc/rpc';
 import { Caller, Tunnel } from './rpc/tunnel';
 
 export * from './api';
-export { Caller, client, provider, Tunnel, MethodType, Rpc };
+export { Caller, client, provider, Tunnel, MethodType, Rpc, utilsApi };
